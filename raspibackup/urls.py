4f895cc73d056bc68ca8a57c6028376d4881b9ad
pgottinger <peter@pgottinger.de>
2013-01-27 19:41:41 +0100

    Update urls.py

--- raspibackup/urls.py
+++ raspibackup/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
-     url(r'^admin/', include(admin.site.urls)),
      url(r'^raspibackup/newbackup/', 'raspibackup.views.new_backup'),
      url(r'^raspibackup/backups/', 'raspibackup.views.backups_overview'),
      url(r'^raspibackup/deletebackup/', 'raspibackup.views.delete_backup'),
